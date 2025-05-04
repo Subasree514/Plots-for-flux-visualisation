08f2c9f194b658c1d04632cd98b406c69cd3c8fb
Subasree Sridhar <ssridhar@uni-koeln.de>
2025-05-04 22:19:17 +0200

    Updated

--- hcc1143wr.m
+++ hcc1143wr.m
@@ -1,41 +1,45 @@
+%% breast cancer models without and with RS
 load('modelHCC1143_rs.mat');
 load('modelHCC1143.mat');
-load('modelHMEL_rs_full.mat')
+
+%% breast non-cancerous models with RS
 load('modelHMEL_rsmin_full.mat')
-%%
-C2 = readmatrix('HMELrsminfullachr.csv');
-C2rs = readmatrix('HCC1143rsachr.csv');
-hcc1143=C2(2:height(C2),2:width(C2));
-hcc1143rs=C2rs(2:height(C2rs),2:width(C2rs));
-%%
-hcc1143rxns=intersect(modelHMEL_rs_full.rxns,modelHCC1143_rs.rxns);
-hcc1143ids_test=findRxnIDs(modelHMEL_rs_full,hcc1143rxns);
-cancer_only=hcc1143(:,hcc1143ids_test);
-hcc1143rsids_test=findRxnIDs(modelHCC1143_rs,hcc1143rxns);
-cancerrs_only=hcc1143rs(:,hcc1143rsids_test);
-for i=1:length(hcc1143ids_test)
+
+%% load rs integrated non-cancerous and cancerous flux samples
+C = readmatrix('HMELrsminfullachr.csv');
+Crs = readmatrix('HCC1143rsachr.csv');
+
+%% get the numerical data (flux samples of reactions)
+breast=C(2:height(C),2:width(C));
+breastrs=Crs(2:height(Crs),2:width(Crs));
+
+%% compare the fluxes of the common reactions
+tnbcrxns=intersect(modelHMEL_rs_full.rxns,modelHCC1143_rs.rxns);
+
+%% non-cancer - sample 1
+tnbcids_test=findRxnIDs(modelHMEL_rs_full,tnbcrxns);
+cancer_only=breast(:,tnbcids_test);
+
+%% cancer - sample 2
+tnbcrsids_test=findRxnIDs(modelHCC1143_rs,tnbcrxns);
+cancerrs_only=breastrs(:,tnbcrsids_test);
+
+%% Wilcoxon rank sum test
+for i=1:length(tnbcids_test)
 [p1_wr(i),h1_wr(i)] = ranksum(cancerrs_only(:,i),cancer_only(:,i));
 end
-h_cancer_wr_single=hcc1143rxns(find(h1_wr==1));
+h_cancer_wr_single=tnbcrxns(find(h1_wr==1));
+
+%% FDR
 [FDR,Q] = mafdr(p1_wr);
 find(Q<0.05);
-h_cancer_wr_multiple=hcc1143rxns(ans);
+h_cancer_wr_multiple=tnbcrxns(ans);
+
+%% Choose that satisfies both single and multiple hypothesis test results
 h_cancer_wr_high_hcc1143=intersect(h_cancer_wr_multiple,h_cancer_wr_single);
-%%
+
+%% flux enrichment analysis of the satistically different reactions
 diff=(h_cancer_wr_high_hcc1143);
 diffids=findRxnIDs(modelHCC1143_rs,diff);
 resultCell = FEA(modelHCC1143_rs,diffids,'subSystems')
 table(resultCell)
-%%
-hcc1143median=median(hcc1143)';
-hcc1143rsmedian=median(hcc1143rs)';
-%%
-%hcc1143rxns=modelHCC1143.rxns;
-hcc1143rsids_test=findRxnIDs(modelHCC1143_rs,diff);
-hcc1143ids_test=findRxnIDs(modelHCC1143,diff);
-cancer_only=hcc1143median(hcc1143ids_test);
-cancerrs_only=hcc1143rsmedian(hcc1143rsids_test);
-fc=cancerrs_only./cancer_only;
-log2(fc);
-table(diff,modelHCC1143_rs.rxnNames(hcc1143rsids_test),cancer_only,cancerrs_only,ans);
-writetable(ans,'hcc1143mediafullnanalysislastco250.xls');
